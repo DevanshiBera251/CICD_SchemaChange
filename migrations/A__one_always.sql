805433a45252481a57c36e7495431f4a08813aeb
DevanshiBera251 <devanshi.r.bera@kipi.bi>
2023-09-21 11:26:14 +0530

    one record added in A file

--- migrations/A__one_always.sql
+++ migrations/A__one_always.sql
@@ -2,4 +2,5 @@
 USE SCHEMA DEMO;
 
 insert into EMPLOYEE(FIRST_NAME,LAST_NAME,CITY,PINCODE,SALARY,AGE) 
-values('Tyler','Dsoza','London','101010','500000',40);
+values('Tyler','Dsoza','London','101010','500000',40),
+('Scott','Hart','Mumbai','202020','700000',45);
