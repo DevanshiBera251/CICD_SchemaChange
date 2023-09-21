50ec55ac910b51f5f80552d973bd94dfc1e014d2
DevanshiBera251 <devanshi.r.bera@kipi.bi>
2023-09-21 11:44:12 +0530

    fun  added into the R file

--- migrations/R__one_repeat.sql
+++ migrations/R__one_repeat.sql
@@ -4,9 +4,7 @@ create or replace function pi_udf()
   returns float
   as '3.14::FLOAT';
 
-CREATE PROCEDURE SelectAllEmployees
-IS
-BEGIN
-SELECT * FROM EMPLOYEE
-END;
+create or replace function i_udf()
+  returns int
+  as '1::INT';
 
