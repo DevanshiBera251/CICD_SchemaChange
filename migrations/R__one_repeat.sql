c3ccf70214698eef2212cbd446df6034a2427467
DevanshiBera251 <devanshi.r.bera@kipi.bi>
2023-09-21 17:39:30 +0530

    check

--- migrations/R__one_repeat.sql
+++ migrations/R__one_repeat.sql
@@ -4,7 +4,7 @@ create or replace function pi_udf()
   returns float
   as '3.14::FLOAT';
 
-create or replace function i_udf()
-  returns int
-  as '1::INT';
+--create or replace function i_udf()
+-- returns int
+ -- as '1::INT';
 
