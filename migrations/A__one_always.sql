e01b7d6fbc7c42ba937afbbaba7be8b150b5328a
DevanshiBera251 <devanshi.r.bera@kipi.bi>
2023-09-21 18:13:06 +0530

    initial

--- migrations/A__one_always.sql
+++
@@ -1,6 +0,0 @@
-
-USE SCHEMA DEMO;
-
-insert into EMPLOYEE(FIRST_NAME,LAST_NAME,CITY,PINCODE,SALARY,AGE) 
-values('Tyler','Dsoza','London','101000','500000',40);
---('Scott','Hart','Mumbai','202020','700000',45);
