4f652f37a593d1a48cacf7d4fdb23108db3bfb7b
DevanshiBera251 <devanshi.r.bera@kipi.bi>
2023-10-13 15:38:05 +0530

    new table ddl

--- migrations/R__one_repeat.sql
+++ migrations/R__one_repeat.sql
@@ -7,3 +7,17 @@ create or replace function pi_udf()
 create or replace function second_udf()
   returns float
   as '3.14::FLOAT';
+
+USE SCHEMA DEMO;
+CREATE TABLE EMPLOYEE
+(
+FIRST_NAME VARCHAR,
+LAST_NAME VARCHAR
+);
+
+USE SCHEMA PUBLIC;
+create TABLE TESTING 
+(
+	FIRST_NAME VARCHAR(16777216),
+	LAST_NAME VARCHAR(16777216)
+);
