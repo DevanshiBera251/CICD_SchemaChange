e3348248942fd8d3506f4ae099da690e5193371d
DevanshiBera251 <devanshi.r.bera@kipi.bi>
2023-11-02 16:10:47 +0530

    val change

--- migrations/R__one_repeat.sql
+++ migrations/R__one_repeat.sql
@@ -1,8 +1,8 @@
 USE SCHEMA PUBLIC;
 
-CREATE or replace view PLATFORM_EVENT_FACT_V_VIEW as select * from Hello_world;
-CREATE or replace view PLATFORM_EVENT_FACT_V_VIEW_NEW as select * from Hello_world;
+--CREATE or replace view PLATFORM_EVENT_FACT_V_VIEW as select * from Hello_world;
+--CREATE or replace view PLATFORM_EVENT_FACT_V_VIEW_NEW as select * from Hello_world;
 
 create or replace function second_udf()
   returns float
-  as '3.14::FLOAT';
+  as '3.11::FLOAT';
