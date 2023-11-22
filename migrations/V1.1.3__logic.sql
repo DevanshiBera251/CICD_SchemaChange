c1105dac5f2341eea1075b64b765984d9bb0a3eb
Yuktha Raman G V <yuktha.r.gv@kipi.bi>
2023-11-22 17:01:53 +0530

    logic test

--- migrations/V1.1.3__logic.sql
+++ migrations/V1.1.3__logic.sql
@@ -1,9 +1,9 @@
 USE SCHEMA REP_VIEW;
 
 BEGIN
-  IF {{warehouse_name}} ='DEV' THEN
+  IF ({{warehouse_name}} ='DEV') THEN
     CREATE or replace TABLE DEV_TBL(FIRST_NAME VARCHAR,LAST_NAME VARCHAR);
-  ELSEIF {{warehouse_name}} ='QA' THEN
+  ELSEIF ({{warehouse_name}} ='QA' )THEN
     CREATE or replace TABLE QA_TBL(FIRST_NAME VARCHAR,LAST_NAME VARCHAR);
   ELSE
     CREATE or replace TABLE PROD_TBL(FIRST_NAME VARCHAR,LAST_NAME VARCHAR);
