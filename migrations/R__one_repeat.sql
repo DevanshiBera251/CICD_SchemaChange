1099a0d06e4c8e998572a89b6017ef9afb871c7d
Yuktha Raman G V <yuktha.r.gv@kipi.bi>
2023-11-28 17:13:21 +0530

    variable test

--- migrations/R__one_repeat.sql
+++ migrations/R__one_repeat.sql
@@ -8,7 +8,7 @@ SELECT * FROM PUBLIC.Hello_world;
 
 
 CREATE OR REPLACE DYNAMIC TABLE {{tab1}}
-LAG = '2 minute'
+LAG = '25 minute'
 WAREHOUSE = {{warehouse_name}}
 AS
 SELECT * FROM PUBLIC.Hello_world;
